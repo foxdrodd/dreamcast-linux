d9f46f7de8ca547c525b58d3f84fd589cefb736b
Florian Fuchs <fuchsfl@gmail.com>
2026-04-25 18:45:20 +0200

    release.sh: also add userlands

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,8 +4,8 @@
 
 # Paths to the built userlands:
 declare -A userland=(
-	[uclibc]=/media/flo/nvme0-ssd/shmusl-uclibc
-	[musl]=/media/flo/nvme0-ssd/shmusl-small2
+	[uclibc]=/media/flo/nvme0-ssd/shuclibc-701
+	[musl]=/media/flo/nvme0-ssd/shmusl-701
 )
 
 linux_version=$(cat dreamcast/build-dreamcast.sh|grep '^LINUX_VERSION=.*'|cut -d"=" -f2|tr -d '"')
@@ -14,6 +14,8 @@ linux_version=$(cat dreamcast/build-dreamcast.sh|grep '^LINUX_VERSION=.*'|cut -d
 ./rebuildkernel.sh
 
 cd build
+reldir=release-${linux_version}
+mkdir -p $reldir
 
 for i in "${!userland[@]}"; do
 	sed -i 's/PRETTY.*/PRETTY_NAME="Dreamcast Linux ('$(date +%Y-%m-%d)')"/' ${userland[$i]}/etc/os-release
@@ -22,13 +24,13 @@ for i in "${!userland[@]}"; do
 	iso=linux-$linux_version-with-userland-$i
 	genisoimage -l -r -C 0,11702 -G IP.BIN -o $iso.iso 1ST_READ.BIN ${userland[$i]}
 	/opt/toolchains/dc/bin/cdi4dc $iso.iso $iso.cdi
-	zstd $iso.cdi -o $iso.cdi.zst
+	zstd $iso.cdi -o $reldir/$iso.cdi.zst
 	usrland=linux-$linux_version-userland-$i
-	zstd ${userland[$i]} -o $usrland-userland.zst
+	tar --zstd -cf $reldir/$usrland.tar.zst -C ${userland[$i]} .
 done
 
-zstd linux616.cdi -o linux-$linux_version-base-busybox.cdi.zst
-zstd 1ST_READ.BIN -o 1ST_READ.BIN.zst
-zstd kernel-boot.bin -o kernel-boot.bin.zst
+zstd linux616.cdi -o $reldir/linux-$linux_version-base-busybox.cdi.zst
+zstd 1ST_READ.BIN -o $reldir/1ST_READ.BIN.zst
+zstd kernel-boot.bin -o $reldir/kernel-boot.bin.zst
 
 cd ..
