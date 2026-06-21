6081343ba8843424401798d1354c83617126687a
Florian Fuchs <fuchsfl@gmail.com>
2026-06-21 21:27:09 +0200

    Tests

---
+++ scripts/release-test.sh
@@ -0,0 +1,14 @@
+# On Release Tests:
+
+# Networking: Mount NFS
+# htop, btop
+# ircii
+# ssh
+# fastfetch with custom logo
+# VMU: mtd  access, info, read, write works
+# VMU: Default Logo is shown
+# VMU: text2lcd works
+# GDROM access works
+# Video mode selection works, something is displayed on the screen, not only serial
+# Does it start a tty on serial and console
+# Does it burn to CDR 700 MB ?
