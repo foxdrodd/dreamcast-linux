4ad3203e8c7a5ffb45aa8846eec44f37ec258c50
Florian Fuchs <fuchsfl@gmail.com>
2026-06-23 00:38:40 +0200

    some tests

--- scripts/release-test.sh
+++ scripts/release-test.sh
@@ -1,14 +1,21 @@
 # On Release Tests:
 
+# Does kernel-boot.bin boot via dcload-serial/gdemu?
+# Does busybox-variant boot via dcload-serial/gdemu?
 # Networking: Mount NFS
 # htop, btop
-# ircii
-# ssh
+# ircii starts
+# fbdoom starts and works in uclibc/musl
+# ssh, telnet starts
 # fastfetch with custom logo
-# VMU: mtd  access, info, read, write works
-# VMU: Default Logo is shown
+# VMU: Does hotplugging memcards work after boot?
+# VMU: mtd access, info, raw read, raw write works
+# VMU: mkfs.vmufat / mount -t vmufat and write files, umount / mount and access
+# VMU: Default Logo is shown on LCD
 # VMU: text2lcd works
 # GDROM access works
 # Video mode selection works, something is displayed on the screen, not only serial
 # Does it start a tty on serial and console
+# Switching consoles work
 # Does it burn to CDR 700 MB ?
+# Do all CDI variants boot?
